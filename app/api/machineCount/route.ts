3044ce3cc24e0a795d0b95453fd7f96d0e77a37f
Tshewang Dechen <12200092.gcit@rub.edu.bt>
2024-06-03 09:31:23 +0600

    changes

--- app/api/machineCount/route.ts
+++ app/api/machineCount/route.ts
@@ -3,12 +3,20 @@ import { NextResponse } from "next/server";
 import Machine from "@/app/models/machine";
 
 export async function GET() {
-    try {
-        await connectMongoDB();
-        const machineCount = await Machine.countDocuments();
-        return NextResponse.json({ count: machineCount }, { status: 200 });
-    } catch (error) {
-        console.error('Error fetching MACHINE count:', error);
-        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
-    }
+  try {
+    console.log("Connecting to MongoDB...");
+    await connectMongoDB();
+    console.log("Connected to MongoDB");
+
+    const machineCount = await Machine.countDocuments();
+    console.log("Machine count fetched:", machineCount);
+
+    return NextResponse.json({ count: machineCount }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching MACHINE count:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
